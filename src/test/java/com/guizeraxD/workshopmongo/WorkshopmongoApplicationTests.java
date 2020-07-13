d4a2b32a098b386384ec3919066cd64f61b70422
Guilherme Tiago <gui-kuunzler@hotmail.com>
2020-07-13 11:18:06 -0300

    Projeto Criado

---
+++ src/test/java/com/guizeraxD/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guizeraxD.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
